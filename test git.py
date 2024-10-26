1416120ee15957e06fd46c254cc23ddee882fad0
bxfb <spectestacc@gmail.com>
2024-10-26 20:16:20 +1000

    ByBit APIs сырой вариант

--- test git.py
+++ test git.py
@@ -1,17 +1,78 @@
-import asyncio
-import json
-from binance import AsyncClient
+from pybit.unified_trading import WebSocket, HTTP
 
-async def main():
-    client = await AsyncClient.create()
 
-    # fetch exchange info
-    res = await client.get_order_book(symbol='BNBBTC')
-    print(json.dumps(res, indent=2))
+ws = WebSocket(
+        testnet=False,
+        channel_type="linear",
+    )
+session = HTTP(testnet=False)
 
-    await client.close_connection()
+
+def handle_message(message):
+    print(message)
+
+
+def OrderBookStream():
+    ws.orderbook_stream(
+        depth=50,
+        symbol="BTCUSDT",
+        callback=handle_message
+    )
+
+
+def TradeStream():
+    ws.trade_stream(
+        symbol="BTCUSDT",
+        callback=handle_message
+    )
+
+
+def TickerStream():
+    ws.ticker_stream(
+        symbol="BTCUSDT",
+        callback=handle_message
+    )
+
+
+def KlineStream():
+    ws.kline_stream(
+        interval=5,
+        symbol="BTCUSDT",
+        callback=handle_message
+    )
+
+
+def LiquidationsStream():
+    ws.liquidation_stream(
+        symbol="BTCUSDT",
+        callback=handle_message
+    )
+
+
+def KlineRequest():
+    print(session.get_kline(
+    category="inverse",
+    symbol="BTCUSDT",
+    interval=60,
+    start=1670601600000,
+    end=1670608800000,
+    ))
+
+
+def OrderBookRequest():
+    print(session.get_orderbook(
+    category="linear",
+    symbol="BTCUSDT",
+    limit=500
+    ))
+
+
+def TickerRequest():
+    print(session.get_tickers(
+        category="inverse",
+        symbol="BTCUSD",
+    ))
+
 
 if __name__ == "__main__":
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    TickerRequest()
