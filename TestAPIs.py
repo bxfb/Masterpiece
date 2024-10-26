9da09572be28795fa3148cdbea97823a4c50241c
bxfb <spectestacc@gmail.com>
2024-10-26 16:19:08 +1000

    Binance APIs сырой вариант

--- TestAPIs.py
+++ TestAPIs.py
@@ -16,10 +16,62 @@ async def OrderBookStream():
     await client.close_connection()
 
 
+async def KlineStream():
+    client = await AsyncClient.create()
+    bm = BinanceSocketManager(client)
+    ts = bm.kline_socket('ETHUSDT', interval=AsyncClient.KLINE_INTERVAL_1MINUTE)
+
+    async with ts as tscm:
+        while True:
+            res = await tscm.recv()
+            print(res)
+
+    await client.close_connection()
+
+
+async def TradeStream():
+    client = await AsyncClient.create()
+    bm = BinanceSocketManager(client)
+    ts = bm.trade_socket('ETHUSDT')
+
+    async with ts as tscm:
+        while True:
+            res = await tscm.recv()
+            print(res)
+
+    await client.close_connection()
+
+
+async def TickerStream():
+    client = await AsyncClient.create()
+    bm = BinanceSocketManager(client)
+    ts = bm.symbol_ticker_socket('ETHUSDT')
+
+    async with ts as tscm:
+        while True:
+            res = await tscm.recv()
+            print(res)
+
+    await client.close_connection()
+
+
+async def MiniTickerStream():
+    client = await AsyncClient.create()
+    bm = BinanceSocketManager(client)
+    ts = bm.miniticker_socket()
+
+    async with ts as tscm:
+        while True:
+            res = await tscm.recv()
+            print(res)
+
+    await client.close_connection()
+
+
 async def OrderBookRequest():
     client = await AsyncClient.create()
 
-    res = await client.get_order_book(symbol='BTCUSDT')
+    res = await client.get_order_book(symbol='ETHUSDT')
     print(json.dumps(res, indent=2))
 
     await client.close_connection()
@@ -37,21 +89,17 @@ async def ExchangeInfo():
 async def SymbolInfo():
     client = await AsyncClient.create()
 
-    res = await client.get_symbol_info('BTCUSDT')
+    res = await client.get_symbol_info('ETHUSDT')
     print(json.dumps(res, indent=2))
 
     await client.close_connection()
 
 
-async def TradeStream():
+async def KlineInfo():
     client = await AsyncClient.create()
-    bm = BinanceSocketManager(client)
-    ts = bm.trade_socket('BTCUSDT')
 
-    async with ts as tscm:
-        while True:
-            res = await tscm.recv()
-            print(res)
+    res = await client.get_klines(symbol='ETHUSDT', interval=AsyncClient.KLINE_INTERVAL_1MINUTE, limit = 2)
+    print(json.dumps(res, indent=2))
 
     await client.close_connection()
 
@@ -59,4 +107,4 @@ async def TradeStream():
 if __name__ == "__main__":
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(TradeStream())
+    loop.run_until_complete(OrderBookStream())
