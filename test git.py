756665d3d12529729564ac50f4fa185c8c4e5a82
bxfb <spectestacc@gmail.com>
2024-10-26 20:46:18 +1000

    Binance APIs сырой вариант

--- test git.py
+++ test git.py
@@ -1,19 +1,15 @@
 from pybit.unified_trading import WebSocket, HTTP
 
 
-ws = WebSocket(
-        testnet=False,
-        channel_type="linear",
-    )
-session = HTTP(testnet=False)
-
-
 def handle_message(message):
     print(message)
 
 
 def OrderBookStream():
-    ws.orderbook_stream(
+    WebSocket(
+        testnet=False,
+        channel_type="linear",
+    ).orderbook_stream(
         depth=50,
         symbol="BTCUSDT",
         callback=handle_message
@@ -21,21 +17,30 @@ def OrderBookStream():
 
 
 def TradeStream():
-    ws.trade_stream(
+    WebSocket(
+        testnet=False,
+        channel_type="linear",
+    ).trade_stream(
         symbol="BTCUSDT",
         callback=handle_message
     )
 
 
 def TickerStream():
-    ws.ticker_stream(
+    WebSocket(
+        testnet=False,
+        channel_type="linear",
+    ).ticker_stream(
         symbol="BTCUSDT",
         callback=handle_message
     )
 
 
 def KlineStream():
-    ws.kline_stream(
+    WebSocket(
+        testnet=False,
+        channel_type="linear",
+    ).kline_stream(
         interval=5,
         symbol="BTCUSDT",
         callback=handle_message
@@ -43,14 +48,17 @@ def KlineStream():
 
 
 def LiquidationsStream():
-    ws.liquidation_stream(
+    WebSocket(
+        testnet=False,
+        channel_type="linear",
+    ).liquidation_stream(
         symbol="BTCUSDT",
         callback=handle_message
     )
 
 
 def KlineRequest():
-    print(session.get_kline(
+    print(HTTP(testnet=False).get_kline(
     category="inverse",
     symbol="BTCUSDT",
     interval=60,
@@ -60,7 +68,7 @@ def KlineRequest():
 
 
 def OrderBookRequest():
-    print(session.get_orderbook(
+    print(HTTP(testnet=False).get_orderbook(
     category="linear",
     symbol="BTCUSDT",
     limit=500
@@ -68,7 +76,7 @@ def OrderBookRequest():
 
 
 def TickerRequest():
-    print(session.get_tickers(
+    print(HTTP(testnet=False).get_tickers(
         category="inverse",
         symbol="BTCUSD",
     ))
