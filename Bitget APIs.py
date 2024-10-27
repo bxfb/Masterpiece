c654c669910bb4cd5e8d042f1b777ce3bafc93f0
bxfb <spectestacc@gmail.com>
2024-10-27 12:54:20 +1000

    Bitget APIs сырой вариант

--- Bitget APIs.py
+++ Bitget APIs.py
@@ -1,4 +1,77 @@
 import requests
+import datetime
+import asyncio
+import json
+import websockets
+
+
+async def TickerStream():
+    url = "wss://ws.bitget.com/v2/ws/public"
+    async with websockets.connect(url, ping_interval=20, ping_timeout=60) as ws:
+        subs = dict(
+            op='subscribe',
+            args=[
+                dict(instType='SPOT', channel='ticker', instId='BTCUSDT')
+            ]
+        )
+        await ws.send(json.dumps(subs))
+
+        async for msg in ws:
+            msg = json.loads(msg)
+            ev = msg.get('event')
+            data = msg.get('data')
+            if ev:
+                print(f'event {ev} = {msg.get('arg')}')
+            elif data and len(data) > 0:
+                symbol = data[0].get('instId')
+                price = float(data[0].get('markPx', '0.0'))
+                print(f'{datetime.datetime.now().isoformat()} {symbol} = {data[0]}')
+
+
+async def TradeStream():
+    url = "wss://ws.bitget.com/v2/ws/public"
+    async with websockets.connect(url, ping_interval=20, ping_timeout=60) as ws:
+        subs = dict(
+            op='subscribe',
+            args=[
+                dict(instType='SPOT', channel='trade', instId='BTCUSDT')
+            ]
+        )
+        await ws.send(json.dumps(subs))
+
+        async for msg in ws:
+            msg = json.loads(msg)
+            ev = msg.get('event')
+            data = msg.get('data')
+            if ev:
+                print(f'event {ev} = {msg.get('arg')}')
+            elif data and len(data) > 0:
+                symbol = data[0].get('instId')
+                price = float(data[0].get('markPx', '0.0'))
+                print(f'{datetime.datetime.now().isoformat()} {symbol} = {data[0]}')
+
+
+async def OrderBookStream():
+    url = "wss://ws.bitget.com/v2/ws/public"
+    async with websockets.connect(url, ping_interval=20, ping_timeout=60) as ws:
+        subs = dict(
+            op='subscribe',
+            args=[
+                dict(instType='SPOT', channel='books15', instId='BTCUSDT')
+            ]
+        )
+        await ws.send(json.dumps(subs))
+
+        async for msg in ws:
+            msg = json.loads(msg)
+            ev = msg.get('event')
+            data = msg.get('data')
+            if ev:
+                print(f'event {ev} = {msg.get('arg')}')
+            elif data and len(data) > 0:
+                symbol = data[0].get('instId')
+                price = float(data[0].get('markPx', '0.0'))
+                print(f'{datetime.datetime.now().isoformat()} {symbol} = {data[0]}')
 
 
 def OrderBookRequest():
@@ -14,4 +87,4 @@ def HistoryKlineRequest():
 
 
 if __name__ == "__main__":
-    HistoryKlineRequest()
+    asyncio.run(OrderBookStream())
