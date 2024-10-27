18afa75cb96e68f36f5c8d4792d6d6d925b638d2
bxfb <spectestacc@gmail.com>
2024-10-27 22:32:04 +1000

    Binance APIs with multistream

--- Binance APIs.py
+++ Binance APIs.py
@@ -4,47 +4,50 @@ import websockets
 from binance import AsyncClient
 
 
-async def MultiStream():
-    url = "wss://stream.binance.com:9443/stream?streams=btcusdt@depth"
-    async with websockets.connect(url, ping_interval=20, ping_timeout=60) as ws:
-        while True:
-            print(json.loads(await ws.recv())['data'])
-
-
 async def OrderBookStream():
     url = "wss://stream.binance.com:9443/stream?streams=btcusdt@depth"
-    async with websockets.connect(url, ping_interval=20, ping_timeout=60) as ws:
+    async with websockets.connect(url) as ws:
         while True:
             print(json.loads(await ws.recv())['data'])
 
 
 async def KlineStream():
     url = "wss://stream.binance.com:9443/stream?streams=btcusdt@kline_1m"
-    async with websockets.connect(url, ping_interval=20, ping_timeout=60) as ws:
+    async with websockets.connect(url) as ws:
         while True:
             print(json.loads(await ws.recv())['data'])
 
 
 async def TradeStreamSpot():
     url = "wss://stream.binance.com:9443/stream?streams=btcusdt@trade"
-    async with websockets.connect(url, ping_interval=20, ping_timeout=60) as ws:
+    async with websockets.connect(url) as ws:
         while True:
             price = json.loads(await ws.recv())['data']['p']
             quantity = json.loads(await ws.recv())['data']['q']
-            buy = json.loads(await ws.recv())['data']['m']
-            return price, quantity, buy
+            is_buy = json.loads(await ws.recv())['data']['m']
+            return price, quantity, is_buy
+
+
+async def TradeStreamFutures():
+    url = "wss://fstream.binance.com/stream?streams=btcusdt@trade"
+    async with websockets.connect(url) as ws:
+        while True:
+            price = json.loads(await ws.recv())['data']['p']
+            quantity = json.loads(await ws.recv())['data']['q']
+            is_buy = json.loads(await ws.recv())['data']['m']
+            print(f"{price} {quantity} {is_buy}")
 
 
 async def TickerStream():
     url = "wss://stream.binance.com:9443/stream?streams=btcusdt@ticker"
-    async with websockets.connect(url, ping_interval=20, ping_timeout=60) as ws:
+    async with websockets.connect(url) as ws:
         while True:
             print(json.loads(await ws.recv())['data'])
 
 
 async def MiniTickerStream():
     url = "wss://stream.binance.com:9443/stream?streams=!miniTicker@arr"
-    async with websockets.connect(url, ping_interval=20, ping_timeout=60) as ws:
+    async with websockets.connect(url) as ws:
         while True:
             print(json.loads(await ws.recv())['data'])
 
@@ -85,12 +88,40 @@ async def KlineInfo():
     await client.close_connection()
 
 
-async def PrintToConsole():
-    while True:
-        price, quantity, buy = await TradeStreamSpot()
-        print(f"{price} {quantity} {buy}")
+async def MultiStream(is_futures: bool):     # Размер свечи корректировать вручную в url
+    if is_futures:
+        url = "wss://fstream.binance.com/stream?streams=btcusdt@depth/btcusdt@kline_1m/btcusdt@trade/btcusdt@ticker"
+    else:
+        url = "wss://stream.binance.com:9443/stream?streams=btcusdt@depth/btcusdt@kline_1m/btcusdt@trade/btcusdt@ticker"
+    async with websockets.connect(url) as ws:
+        while True:
+            msg = json.loads(await ws.recv())
+            if msg['stream'] == "btcusdt@depth":
+                bids = msg['data']['b']
+                asks = msg['data']['a']
+            elif msg['stream'][:-3] == "btcusdt@kline":
+                open_price_kline = msg['data']['k']['o']
+                close_price_kline = msg['data']['k']['c']
+                high_price = msg['data']['k']['h']
+                low_price = msg['data']['k']['l']
+                base_asset_volume = msg['data']['k']['v']
+                quote_asset_volume = msg['data']['k']['q']
+                taker_buy_base_asset_volume = msg['data']['k']['V']
+                taker_buy_quote_asset_volume = msg['data']['k']['Q']
+            elif msg['stream'] == "btcusdt@trade":
+                price = msg['data']['p']
+                quantity = msg['data']['q']
+                is_buy = msg['data']['m']
+            elif msg['stream'] == "btcusdt@ticker":
+                price_change = msg['data']['p']
+                price_change_percent = msg['data']['P']
+                weighted_average_price = msg['data']['w']
+                open_price_ticker = msg['data']['o']
+                last_price_ticker = msg['data']['c']
+                total_traded_base_asset_volume = msg['data']['v']
+                total_traded_quote_asset_volume = msg['data']['q']
 
 
 if __name__ == "__main__":
 
-    asyncio.run(PrintToConsole())
+    asyncio.run(MultiStream(True))
